f4c8cdeabb69c65098ee08b89f594c62e4aa1d37
Ahmad Raza <ahmadrazach26@gmail.com>
2022-02-14 17:12:43 +0500

    done with logic of hurdleRace.py

---
+++ Algorithms/hurdleRace.py
@@ -0,0 +1,20 @@
+#
+# Complete the 'hurdleRace' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. INTEGER_ARRAY height
+#
+
+def hurdleRace(k, height):
+    # Write your code here
+    # find max from the k
+
+    # if max_k < =height:
+    # return 0
+    # else:
+    # return max(k)-height
+
+
+print(hurdleRace([1, 6, 3, 5, 2], 5))
