b367c8150a637d1f795374b962bef298e45eb1ba
Ahmad Raza <ahmadrazach26@gmail.com>
2022-02-14 18:48:26 +0500

    create formingMagicSqaure.py
    Done with the logic of it

---
+++ Algorithms/formingMagicSquare.py
@@ -0,0 +1,17 @@
+#
+# Complete the 'formingMagicSquare' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY s as parameter.
+#
+
+def formingMagicSquare(s):
+    # Write your code here
+    # convert it into 2d array
+
+    # take all the magic syare
+    # find the diffenerce between each MATRIX FROM MAGIC AND MAGIC SUARE
+    print(s)
+
+
+print(formingMagicSquare([5, 3, 4, 1, 5, 8, 6, 4, 2]))
