ade65f676875e064dc7f94eeb80b1ee2d5b35f2b
Ahmad Raza <ahmadrazach26@gmail.com>
2022-02-15 17:28:29 +0500

    create pickingNumbers.py

---
+++ Algorithms/pickingNukbers.py
@@ -0,0 +1,44 @@
+#
+# Complete the 'pickingNumbers' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def pickingNumbers(a):
+    # Write your code here
+    # a_list = []
+    max = 0
+    count = 0
+    # print(a)
+    # # loop till the all of a
+    # for i in a:
+    #     # loop with inner loop
+    #     for j in range(i+1, len(a)):
+    #         # for
+    #         print(i, a[j], " = ", abs(i-a[j]), end=" ")
+    #         if(abs(i-j) <= 1):
+    #             print("in ")
+    # print()
+    #             a_list.append(a[i])
+    #         print()
+    # print("\n List =", a_list)
+    pass
+    # maxlength=1
+    # check all possible subarrays for i in rane(n):
+    # #initalize minimum and maximum of current subarray
+    # #minOfSub=a[i]
+    # #maxOfSub=a[i]
+    # #loop from i+1 till n
+    #     #update max and min values if conditions meet
+    #     #if(arr[j]>maxOfSub)
+    #         #maxOfSub=arr[j]
+    #     if(arr[j]<minOfSub)
+    #         minOfSub=arr[j]
+
+    #     #check if current subarray satisfies the condition
+    #     if(maxofSub-minOfSub)<=k:
+    #         curr
+
+
+pickingNumbers([1, 1, 2, 2, 4, 4, 5, 5, 5])
