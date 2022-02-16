950c2875a6eb7e6262509a79fde7152d5839854f
Ahmad Raza <ahmadrazach26@gmail.com>
2022-02-17 01:25:56 +0500

    Done with logic of climbingLeaderboard.py

---
+++ Algorithms/climbingLeaderboard.py
@@ -0,0 +1,27 @@
+#
+# Complete the 'climbingLeaderboard' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY ranked
+#  2. INTEGER_ARRAY player
+#
+
+def climbingLeaderboard(ranked, player):
+    # Write your code here
+    # player_score=[]
+    # loop till the player
+    # rank
+    # make templist and equal it to original list
+    # append the element to the of player[i] to the temp list
+    # sort the list
+    # for loop till the templ_list
+    # if temp_list[i]!=temp_list or temp_list[i]==len(temp_list)
+    # rank+=1
+    # if temp_list[i]==player[i]
+    # player_score.append(rank)
+
+    print(player)
+
+
+climbingLeaderboard([100, 100, 50, 40, 40, 20, 10], [5, 25, 50, 120])
