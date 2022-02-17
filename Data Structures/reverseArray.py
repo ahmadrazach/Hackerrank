d609bebceef1e93e4bd0e22774ec434b41650424
Ahmad Raza <ahmadrazach26@gmail.com>
2022-02-17 15:12:31 +0500

    Update reverseArray.py
    Done with the question and testing test cases!

--- Data Structures/reverseArray.py
+++ Data Structures/reverseArray.py
@@ -8,8 +8,15 @@
 def reverseArray(a):
     # Write your code here
     # toop till the full array
-    # take the first elment and add it to the temp_array
-    # remove element from that index
+    temp_array = []
+    for index in range(len(a)-1, -1, -1):
+        # take the first elment and add it to the temp_array
+        temp_array.append(a[index])
+        # remove element from that index
+        # print(a[index])
+        # a.append(a[index])
+
+    return(temp_array)
 
 
-reverseArray([1, 4, 3, 2])
+print(reverseArray([1, 4, 3, 2]))
