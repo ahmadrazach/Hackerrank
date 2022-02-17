2d12a665d1853e631e778b050ba9082f559297ca
Ahmad Raza <ahmadrazach26@gmail.com>
2022-02-17 14:51:27 +0500

    create reverseArray.py
    Made the logic

---
+++ Data Structures/reverseArray.py
@@ -0,0 +1,15 @@
+#
+# Complete the 'reverseArray' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def reverseArray(a):
+    # Write your code here
+    # toop till the full array
+    # take the first elment and add it to the temp_array
+    # remove element from that index
+
+
+reverseArray([1, 4, 3, 2])
