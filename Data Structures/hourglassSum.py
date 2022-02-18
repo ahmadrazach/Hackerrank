2e84d691bbab6d4e21642d8ff64221563abc323e
Ahmad Raza <ahmadrazach26@gmail.com>
2022-02-18 17:28:33 +0500

    done with logic hourglassSum.py

--- Data Structures/hourglassSum.py
+++ Data Structures/hourglassSum.py
@@ -7,12 +7,74 @@
 
 def hourglassSum(arr):
     # Write your code here
+    max_square_sum = 0
+    # 1st section
+    for square_count in range(0, 4):
+        #print("count = ", square_count)
+        count_square_sum = 0
+        for r in range(0, 3):
+
+            r1 = 0
+            for c in range(0+square_count, 3+square_count):
+                if r != 1:
+                    count_square_sum += arr[r][c]
+                    print(arr[r][c], end=" ")
+
+                # agr second row hey
+                if r == 1:
+                    # count hwo much times second row runs
+                    if r1 == 1:
+                        print(" ", arr[r][c], end=" ")
+                        count_square_sum += arr[r][c]
+                    r1 += 1
+             #   print(arr[r][c], end=" ")
+            print()
+        max_square_sum = max(max_square_sum, count_square_sum)
+        print("Max sum Square =", max_square_sum)
+    # 2nd section row
+    for square_count in range(0, 4):
+        #print("count = ", square_count)
+        for r in range(1, 4):
+            for c in range(0+square_count, 3+square_count):
+                if r != 1:
+                    print(arr[r][c], end=" ")
+
+                # agr second row hey
+                if r == 1:
+                    # count hwo much times second row runs
+                    if r1 == 1:
+                        print(" ", arr[r][c], end=" ")
+                    r1 += 1
+            print()
+    # 3rd section row
     for square_count in range(0, 4):
         print("count = ", square_count)
-        for r in range(0, 3):
+        for r in range(2, 5):
             for c in range(0+square_count, 3+square_count):
+                if r != 1:
+                    print(arr[r][c], end=" ")
 
-                print(arr[r][c], end=" ")
+                # agr second row hey
+                if r == 1:
+                    # count hwo much times second row runs
+                    if r1 == 1:
+                        print(" ", arr[r][c], end=" ")
+                    r1 += 1
+            print()
+    # 4rth sectio row
+    for square_count in range(0, 4):
+        print("count = ", square_count)
+        for r in range(3, 6):
+            for c in range(0+square_count, 3+square_count):
+                if r != 1:
+                    print(arr[r][c], end=" ")
+
+                # agr second row hey
+                if r == 1:
+                    # count hwo much times second row runs
+                    if r1 == 1:
+                        print(" ", arr[r][c], end=" ")
+                    r1 += 1
             print()
 
 
