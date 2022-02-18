21f78df1f6ec946e552322d1a957cd561e0409a9
Ahmad Raza <ahmadrazach26@gmail.com>
2022-02-18 17:32:52 +0500

    Done with the logic hourglassSum.py

--- Data Structures/hourglassSum.py
+++ Data Structures/hourglassSum.py
@@ -34,9 +34,12 @@ def hourglassSum(arr):
     # 2nd section row
     for square_count in range(0, 4):
         #print("count = ", square_count)
+        count_square_sum = 0
         for r in range(1, 4):
+            r1 = 0
             for c in range(0+square_count, 3+square_count):
                 if r != 1:
+                    count_square_sum += arr[r][c]
                     print(arr[r][c], end=" ")
 
                 # agr second row hey
@@ -44,14 +47,21 @@ def hourglassSum(arr):
                     # count hwo much times second row runs
                     if r1 == 1:
                         print(" ", arr[r][c], end=" ")
+                        count_square_sum += arr[r][c]
                     r1 += 1
+             #   print(arr[r][c], end=" ")
             print()
+        max_square_sum = max(max_square_sum, count_square_sum)
+        print("Max sum Square =", max_square_sum)
     # 3rd section row
     for square_count in range(0, 4):
         print("count = ", square_count)
+        count_square_sum = 0
         for r in range(2, 5):
+            r1 = 0
             for c in range(0+square_count, 3+square_count):
                 if r != 1:
+                    count_square_sum += arr[r][c]
                     print(arr[r][c], end=" ")
 
                 # agr second row hey
@@ -59,14 +69,21 @@ def hourglassSum(arr):
                     # count hwo much times second row runs
                     if r1 == 1:
                         print(" ", arr[r][c], end=" ")
+                        count_square_sum += arr[r][c]
                     r1 += 1
+             #   print(arr[r][c], end=" ")
             print()
+        max_square_sum = max(max_square_sum, count_square_sum)
+        print("Max sum Square =", max_square_sum)
     # 4rth sectio row
     for square_count in range(0, 4):
         print("count = ", square_count)
+        count_square_sum = 0
         for r in range(3, 6):
+            r1 = 0
             for c in range(0+square_count, 3+square_count):
                 if r != 1:
+                    count_square_sum += arr[r][c]
                     print(arr[r][c], end=" ")
 
                 # agr second row hey
@@ -74,8 +91,12 @@ def hourglassSum(arr):
                     # count hwo much times second row runs
                     if r1 == 1:
                         print(" ", arr[r][c], end=" ")
+                        count_square_sum += arr[r][c]
                     r1 += 1
+             #   print(arr[r][c], end=" ")
             print()
+        max_square_sum = max(max_square_sum, count_square_sum)
+        print("Max sum Square =", max_square_sum)
 
 
 hourglassSum([[1, 1, 1, 0, 0, 0],
