6458efb9761eb9a464b2b3ba5e15f8097c739391
Ahmad Raza <ahmadrazach26@gmail.com>
2022-03-15 16:59:42 +0500

    Done with the solution of it

--- Algorithms/miniMaxSum.py
+++ Algorithms/miniMaxSum.py
@@ -9,14 +9,30 @@ from sys import maxsize
 def miniMaxSum(arr):
     # Write your code here
     min = 0
-    max = maxsize
-    sum = 0
+    max = 0
+
     # loop i till the range
-    # loop j till the range
-    # calculate sum if(i!=j)
-    ##if sum<min : min=sum
-    # elif sum>max: max=sum
+    for i in range(len(arr)):
+        # loop j till the range
+        sum = 0
+        print(i)
+        for j in range(len(arr)):
+            # calculate sum if(i!=j)
+            if(i != j):
+                print(arr[j], end=" ")
+                sum += arr[j]
+            ##if sum<min : min=sum
+
+            # elif sum>max: max=sum
+        if(i == 0):
+            min = sum
+            max = sum
+        else:
+            if(sum < min):
+                min = sum
+            if(sum > max):
+                max = sum
     print(min, " ", max)
 
 
-miniMaxSum([1, 3, 5, 7, 9])
+miniMaxSum([1, 2, 3, 4, 5])
