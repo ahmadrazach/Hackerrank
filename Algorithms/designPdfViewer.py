a22c61e91eb2ab817e3a650974aaf9458dac492f
Ahmad Raza <ahmadrazach26@gmail.com>
2022-03-18 18:50:53 +0500

    Done with the question

--- Algorithms/designPdfViewer.py
+++ Algorithms/designPdfViewer.py
@@ -18,10 +18,35 @@ import sys
 
 def designerPdfViewer(h, word):
     # Write your code here
-    print(h)
-    
-    print(word)
+    # making a array of all characters
+    char_array = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+                  "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    # spliting word string in an array
+    word_array = list(word)
+    print(word_array)
+
+    # finding ACII calue of a char and indexing them 0-25
+    ascii_array = []
+    for i in range(len(word_array)):
+        ascii_array.append(ord(str(word_array[i]))-97)
+    # removing duplicate values
+    ascii_array = list(dict.fromkeys(ascii_array))
+    # sorting the array
+    ascii_array.sort()
+    # print(ascii_array)
+    # loop to compare the value and save the given char heights
+    letter_heights = []
+    count = 0
+    for i in range(len(h)):
+        # print(i)
+        if(count < len(ascii_array)):
+            if(i == ascii_array[count]):
+                count += 1
+                letter_heights.append(h[i])
+                # print(h[i])
+    print(max(letter_heights)*len(word_array))
+    # print(word_array)
 
 
 designerPdfViewer([1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5,
-                  5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5], "abc")
+                  5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7], "zaba")
