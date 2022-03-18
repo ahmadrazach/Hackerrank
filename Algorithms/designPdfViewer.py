34831e5b51b278f39eb7211967b285331adcc00e
Ahmad Raza <ahmadrazach26@gmail.com>
2022-03-18 16:15:09 +0500

    initial settings of designPdfViewer

---
+++ Algorithms/designPdfViewer.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'designerPdfViewer' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY h
+#  2. STRING word
+#
+
+
+def designerPdfViewer(h, word):
+    # Write your code here
+    print(h)
+    
+    print(word)
+
+
+designerPdfViewer([1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5,
+                  5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5], "abc")
