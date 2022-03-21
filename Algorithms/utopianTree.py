2c32fd983f7258636b7d81c33122afa412c4b5b4
Ahmad Raza <ahmadrazach26@gmail.com>
2022-03-21 20:39:09 +0500

    Done with the logic

--- Algorithms/utopianTree.py
+++ Algorithms/utopianTree.py
@@ -9,13 +9,25 @@ def utopianTree(n):
     # Write your code here
     # for i in len(int(n)):
     # print(n)
-    height=1
-    for i in range(0, n):
-        print(i)
+    height = 0
+    for i in range(0, n+1):
+        # print(i)
+        # print(i)
+        # pass
         # if odd (1,3,5,7,9)
-        # height+=1
+        if (i % 2 != 0):
+            # height*=1
+            if(i == 1):
+                height = 2
+            else:
+                height *= 2
+            print("odd :", height)
         # if even (2,4,6,8)
-        # height*=2
+        else:
+            height += 1
+            print("even :", height)
+        # height+=2
+    return(height)
 
 
-utopianTree(3)
+print(utopianTree(5))
