75cb58e565f99d312b20e2e54b24f01519f70567
Ahmad Raza <ahmadrazach26@gmail.com>
2022-03-21 19:32:57 +0500

    Done with UptionTree logic

---
+++ Algorithms/utopianTree.py
@@ -0,0 +1,14 @@
+#
+# Complete the 'utopianTree' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def utopianTree(n):
+    # Write your code here
+    for i in n:
+        print(i)
+
+
+utopianTree(3)
