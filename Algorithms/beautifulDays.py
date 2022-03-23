2798facecd2d606cfea03f2ee42e3c02dd318d63
Ahmad Raza <ahmadrazach26@gmail.com>
2022-03-23 18:17:56 +0500

    started working on beautifulDays

---
+++ Algorithms/beautifulDays.py
@@ -0,0 +1,16 @@
+#
+# Complete the 'beautifulDays' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER i
+#  2. INTEGER j
+#  3. INTEGER k
+#
+
+def beautifulDays(i, j, k):
+    # Write your code here
+    return k
+
+print(beautifulDays(1,2,3))
+
