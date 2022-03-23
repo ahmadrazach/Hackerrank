9182f5c4fed8e047d0e139ab02db6770bbc5ffbf
Ahmad Raza <ahmadrazach26@gmail.com>
2022-03-23 18:07:56 +0500

    started working on angryProfessor question

---
+++ Algorithms/angryProfessor.py
@@ -0,0 +1,15 @@
+#
+# Complete the 'angryProfessor' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. INTEGER_ARRAY a
+#
+
+def angryProfessor(k, a):
+    # Write your code here
+    
+    return "YES"
+
+print(angryProfessor(3,[-1,-3,4,2]))
