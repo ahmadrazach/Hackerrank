5c33b2089099a3a6eb24d52b50658863971e06b1
Ahmad Raza <ahmadrazach26@gmail.com>
2022-03-23 18:11:22 +0500

    Done with the logic

--- Algorithms/angryProfessor.py
+++ Algorithms/angryProfessor.py
@@ -9,12 +9,18 @@
 
 def angryProfessor(k, a):
     # Write your code here
+    onTime=0
     # loop till the lenth of array
+    for i in a:
     # count the negative values
+        if(i<=0):
+            onTime+=1
     # if negative values greater than or equal to thresholg
+    if(onTime<k):
     # return yes
+        return "YES"
     # else NO
-    return "YES"
+    return "NO"
 
 
-print(angryProfessor(3, [-1, -3, 4, 2]))
+print(angryProfessor(2, [0, -1, 2, 1]))
