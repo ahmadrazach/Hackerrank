956f0dd2a73f8ea40ccf56e882266744e528fdd7
Ahmad Raza <ahmadrazach26@gmail.com>
2022-03-24 18:32:38 +0500

    done with the logic

--- Algorithms/viralAdvertising.py
+++ Algorithms/viralAdvertising.py
@@ -7,6 +7,21 @@
 
 def viralAdvertising(n):
     # Write your code here
+
+# Day Shared Liked Cumulative
+# 1      5     2       2
+# 2      6     3       5
+# 3      9     4       9
+# 4     12     6      15
+# 5     18     9      24
+#   cummilative=0
+    #sahred_people = 0
+    #loop till the days
+    #like_people=floor(people/2)
+    # cummulative+=liked_people
+    # shared_people=like_people*3
+    
+    # 
     return n
 
 
