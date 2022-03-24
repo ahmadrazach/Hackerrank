6f68828d3a6b76bffd3a2a38449c42c810d61580
Ahmad Raza <ahmadrazach26@gmail.com>
2022-03-24 18:12:31 +0500

    started working on viralAcademy.py

---
+++ Algorithms/viralAdvertising.py
@@ -0,0 +1,13 @@
+#
+# Complete the 'viralAdvertising' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def viralAdvertising(n):
+    # Write your code here
+    return n
+
+
+print(viralAdvertising(10))
