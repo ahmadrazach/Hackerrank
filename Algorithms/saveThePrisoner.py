6b3e657d9fd0fab43f899a87d2eb6482981762b1
Ahmad Raza <ahmadrazach26@gmail.com>
2022-03-26 01:09:19 +0500

    done with logic of saveThePrisoner.py

---
+++ Algorithms/saveThePrisoner.py
@@ -0,0 +1,22 @@
+#
+# Complete the 'saveThePrisoner' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER m
+#  3. INTEGER s
+#
+
+def saveThePrisoner(n, m, s):
+    # Write your code here
+    # count=0
+    # chair_no=s
+    # while (count!=m)
+    #count+=1
+    #if(prisoner_no==n)
+    #chair_no=1
+    #else: chari_no+=1
+
+
+print(saveThePrisoner(5, 2, 1))
