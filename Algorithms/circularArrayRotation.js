cd6f89352348a38952d47409bcb86fd30465683d
Ahmad Raza <ahmadrazach26@gmail.com>
2022-03-27 13:30:24 +0500

    made the logic

--- Algorithms/circularArrayRotation.js
+++ Algorithms/circularArrayRotation.js
@@ -1,5 +1,9 @@
 function circularArray(n){
-    return n
+    //console.log(());
+    console.log(n)
+    //to take a variable from last and save on first
+    console.log(n.unshift(n.pop(n)))
+    return n ;
 }
 
-console.log(circularArray(10))
+console.log(circularArray([1,2,3,4,5]))
