01f641838b2aec1b6b3b2e175b6f655e1f89ddc3
Ahmad Raza <ahmadrazach26@gmail.com>
2022-04-04 16:43:25 +0500

    Update extraLongFactorial.js

--- Algorithms/extraLongFactorial.js
+++ Algorithms/extraLongFactorial.js
@@ -8,7 +8,18 @@ function extraLongFactorials(n) {
     // Write your code here
     //loop till the 0,start from size,i--
     //multiple value with saved value
-    return n
+
+    let result=1
+    count=1
+    while(count<n)
+    {
+        count++
+        result=result*count
+        //console.log(count,result)
+        
+    }
+    
+    return BigInt(n)
 }
 
-console.log(extraLongFactorials(4))
+console.log(extraLongFactorials(30))
