2e442dc2e0f45a45bc2a22f10c7277928663bba4
Ahmad Raza <ahmadrazach26@gmail.com>
2022-04-04 16:43:26 +0500

    Create extraLongFactorial.py

---
+++ Algorithms/extraLongFactorial.py
@@ -0,0 +1,20 @@
+#
+# Complete the 'extraLongFactorials' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+
+def extraLongFactorials(n):
+    # Write your code here
+    result=1
+    count=1
+    while(count<n):
+        count+=1
+        result=result*count
+        #console.log(count,result)
+        
+    
+    
+       
+
+extraLongFactorials(25)
