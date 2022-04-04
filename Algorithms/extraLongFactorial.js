59ebc01718cfa67ae3d041e7edc6a6c783a5fddc
Ahmad Raza <ahmadrazach26@gmail.com>
2022-04-04 15:44:06 +0500

    started working on extraLongFactorial

---
+++ Algorithms/extraLongFactorial.js
@@ -0,0 +1,14 @@
+/*
+ * Complete the 'extraLongFactorials' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+function extraLongFactorials(n) {
+    // Write your code here
+    //loop till the 0,start from size,i--
+    //multiple value with saved value
+    return n
+}
+
+console.log(extraLongFactorials(4))
