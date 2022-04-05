0223a4741b3608b6b04b6e770d800e1a40c92e8a
Ahmad Raza <ahmadrazach26@gmail.com>
2022-04-05 15:55:08 +0500

    started working on the squares

---
+++ Algorithms/squares.js
@@ -0,0 +1,17 @@
+/*
+ * Complete the 'squares' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER a
+ *  2. INTEGER b
+ */
+
+function squares(a, b) {
+    // Write your code here
+    //loop till all
+    //square root ans in int?count++:null
+    //return number
+}
+
+console.log(sqaures(24,49))
