6f51a69680c2c380564dcfc564010da072d3d67a
Ahmad Raza <ahmadrazach26@gmail.com>
2022-04-05 16:01:26 +0500

    done with the question

--- Algorithms/squares.js
+++ Algorithms/squares.js
@@ -10,8 +10,12 @@
 function squares(a, b) {
     // Write your code here
     //loop till all
+    count=0
+    for(let i=a;i<=b;i++)
+        Math.sqrt(i)==Math.round(Math.sqrt(i))?count+=1:null;
     //square root ans in int?count++:null
     //return number
+    return count
 }
 
-console.log(sqaures(24,49))
+console.log(squares(24,49))
