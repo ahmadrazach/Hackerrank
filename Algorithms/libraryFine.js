b84d12727c39cc4824c74c0e13296b208ea051f8
Ahmad Raza <ahmadrazach26@gmail.com>
2022-04-06 16:25:14 +0500

    starte with the librayfine

---
+++ Algorithms/libraryFine.js
@@ -0,0 +1,21 @@
+/*
+ * Complete the 'libraryFine' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER d1
+ *  2. INTEGER m1
+ *  3. INTEGER y1
+ *  4. INTEGER d2
+ *  5. INTEGER m2
+ *  6. INTEGER y2
+ */
+
+function libraryFine(d1, m1, y1, d2, m2, y2) {
+    // Write your code here
+    let fine =0
+    // (y2===y1 ? (m2 === m1?( d2===d1 ?fine=fine :15*(d2-d1) ): ):y2>y1)
+    return d1
+}
+
+console.log(libraryFine(14,7,2018,5,7,2018))
