561254f7a363ee4b1408b7572129e14478a88f48
Ahmad Raza <ahmadrazach26@gmail.com>
2022-04-07 17:29:31 +0500

    started cutTheSticks

---
+++ Algorithms/CutTheSticks.js
@@ -0,0 +1,14 @@
+/*
+ * Complete the 'cutTheSticks' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function cutTheSticks(arr) {
+    // Write your code here
+    while ()
+    return arr
+}
+
+console.log(cutTheSticks([1,2,3]))
