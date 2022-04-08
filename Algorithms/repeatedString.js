dff30c9bc895c898872e725a77a9c2e41c666576
Ahmad Raza <ahmadrazach26@gmail.com>
2022-04-08 17:27:37 +0500

    it's passing 3 test cases for now!

--- Algorithms/repeatedString.js
+++ Algorithms/repeatedString.js
@@ -11,10 +11,27 @@ function repeatedString(s, n) {
     // Write your code here
     //check if the s has only one number and is equal to the n
     //->return n
-    //append the numbers to the
-    let matching_num=/a/g; 
+    //take the length of the string s
+    let matching_num=/a/g; //regular expression for matching
+    if(s.match(matching_num).length===s.length){
+        return n
+    }
+    const string_length=s.length;
+    let copiedstring=s;
+    let s_index=0
+
+     //loop to give length and apeending the string s to size n
+    while(s.length<n){
+        //pushing value of s into the s 
+        s+=s[s_index]
+        console.log('aya',s.length,s)
+        s_index++
+    }
+    console.log(s.length,n)
+    console.log(s)
+   
     return s.match(matching_num).length
 }
 
 
-console.log(repeatedString('aba',10))
+console.log(repeatedString('ababa',3))
