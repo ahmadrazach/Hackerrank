e286b8df5e10ce5cde6e7db1d854a7d5a942dcc2
Ahmad Raza <ahmadrazach26@gmail.com>
2022-04-12 16:47:49 +0500

    starting workin gon jumpingClouds after making logic

---
+++ Algorithms/jumpingClouds.js
@@ -0,0 +1,15 @@
+/*
+ * Complete the 'jumpingOnClouds' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY c as parameter.
+ */
+
+function jumpingOnClouds(c) {
+    // Write your code here
+    let steps=0
+    let cloud_index=0
+    while()
+return c
+}
+console.log(jumpingOnClouds([0,1,0,0,0,1,0]))
