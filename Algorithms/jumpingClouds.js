cd4d0de9a84a10bd174415398f948dfab3755616
Ahmad Raza <ahmadrazach26@gmail.com>
2022-04-12 16:55:52 +0500

    Done with cheching both test cases

--- Algorithms/jumpingClouds.js
+++ Algorithms/jumpingClouds.js
@@ -9,7 +9,17 @@ function jumpingOnClouds(c) {
     // Write your code here
     let steps=0
     let cloud_index=0
-    while()
-return c
+    while(cloud_index<c.length-1){
+        if(c[cloud_index+2]===0)
+        {
+            steps++;cloud_index+=2
+        }
+        else if(c[cloud_index+1]===0)
+        {
+            steps++;cloud_index++
+        }
+        // console.log(steps,cloud_index)
+    }
+return steps
 }
-console.log(jumpingOnClouds([0,1,0,0,0,1,0]))
+console.log(jumpingOnClouds([0, 0, 1, 0, 0, 1, 0]))
