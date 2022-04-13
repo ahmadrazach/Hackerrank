c6984ac56757c89e49d82dd5bd2ed10a86c6435f
Ahmad Raza <ahmadrazach26@gmail.com>
2022-04-13 16:32:17 +0500

    started working on the equalizeArray and made the logic

---
+++ Algorithms/equalizeArray.js
@@ -0,0 +1,18 @@
+/*
+ * Complete the 'equalizeArray' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function equalizeArray(arr) {
+    // Write your code here
+    
+    //sort the array
+    arr.sort((a,b)=>a-b)
+    return PluginArray
+    //if the next index is not same?count++:
+
+}
+
+console.log(equalizeArray([1,2,2,3]))
