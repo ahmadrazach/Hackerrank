add72139f317d979832e4daa059a1023db724c24
Ahmad Raza <ahmadrazach26@gmail.com>
2022-04-18 20:19:31 +0500

    example cases passed but other failed

--- Algorithms/taumBday.js
+++ Algorithms/taumBday.js
@@ -13,10 +13,28 @@
 function taumBday(b, w, bc, wc, z) {
     // Write your code here
     // bc+z===wc || bc===wc -> return b*bc+w*wc
+    if(bc+z===wc || bc===wc) {
+        // console.log('equals')
+        return b*bc+w*wc}
     // bc+z>wc ->return b*bc+w*wc
+    else if(bc+z>wc && z+wc>bc){
+        // console.log('bc+z>wc')
+        return b*bc+w*wc}
+    
     //? bc>wc+z -> return (b+w)*1+b*1
+    else if(bc>wc+z && b!=w) {
+        // console.log('this b!=w')
+        return (b+w)*z+b*z} 
     // bc>wc+z -> return w*(wc+z)+w*z 6=6
+    else if(bc>wc+z && b===w) {
+        // console.log('this')
+        return w*(wc+z)+w*wc} 
     // wc>bc+z -> return b*(bc+z)+b*bc 9+6=15
+    else if( wc>bc+z){
+        // console.log('wc>bc+z')
+        return b*(bc+z)+b*bc
+    }
+
 }
 
-console.log(taumBday(3,5,3,4,1))
+console.log(taumBday(5,9,2,3,4))
