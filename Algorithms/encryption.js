33ceb0384aeceef98e2e3f5b294f3af0f52131d4
Ahmad Raza <ahmadrazach26@gmail.com>
2022-05-10 11:21:10 +0500

    done with solving all the testcases

--- Algorithms/encryption.js
+++ Algorithms/encryption.js
@@ -7,34 +7,47 @@
 
 function encryption(s) {
     // Write your code here
-    
-    //1. remove spaces from string
-    console.log(s.replace(/\s+/g,''))
-    s=s.replace(/\s+/g,'')
-    // console.log(s)
-    //2. taking  square root and finding length
-    let row=parseInt(Math.sqrt(s.length))
-    let col=row+1
-    //3. looping 
-    let result_string="";
-    let temp_index=0
-    for(let index=0;index<col;index++){
-        temp_index=index
-        result_string+=s[temp_index]
-        
-        for(let i=0;i<row;i++)
+    if(s.length>1)
+    {    //1. remove spaces from string
+        s=s.replace(/\s+/g,'')
+        console.log(s)
+        //2. taking  square root and finding length
+        sqr_root=Math.sqrt(s.length)
+        let row=0;
+        if(Number.isInteger(Math.sqrt(s.length)))
         {
-            temp_index+=col
-            // console.log(temp_index,s[temp_index])
-            if(temp_index<s.length)
-            {result_string+=s[temp_index]}
+            row=parseInt(sqr_root)-1;
+        }
+        else{
+            row=parseInt(sqr_root)
+        }
+        
+        let col=row+1
+        console.log((Math.sqrt(s.length)))
+        //3. looping 
+        let result_string="";
+        let temp_index=0
+        for(let index=0;index<col;index++){
+            temp_index=index
+            result_string+=s[temp_index]
+            
+            for(let i=0;i<row;i++)
+            {
+                temp_index+=col
+                // console.log(temp_index,s[temp_index])
+                if(temp_index<s.length)
+                {result_string+=s[temp_index]}
+            }
+
+            result_string+=" ";
+            console.log(result_string)
         }
 
-        result_string+=" ";
-        console.log(result_string)
+        return result_string
     }
+    return s
     
-    return result_string
+    
 }
 
 console.log(encryption("haveaniceday"))
