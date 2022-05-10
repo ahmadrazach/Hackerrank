c20eb766135f8952920162f8403e4fc03b4d5533
Ahmad Raza <ahmadrazach26@gmail.com>
2022-05-10 10:58:36 +0500

    Done with the logic

--- Algorithms/encryption.js
+++ Algorithms/encryption.js
@@ -7,21 +7,34 @@
 
 function encryption(s) {
     // Write your code here
-    //spliting the string to arrays of
-    // console.log(parseInt(Math.sqrt()))
+    
+    //1. remove spaces from string
+    console.log(s.replace(/\s+/g,''))
+    s=s.replace(/\s+/g,'')
+    // console.log(s)
+    //2. taking  square root and finding length
     let row=parseInt(Math.sqrt(s.length))
     let col=row+1
-    console.log(row,col)
-    for(let i=0;i<row;i++){
-        for(let j=0;j<col;j++)
-        {
-            console.log(s,s.substr(i,j))
-        }
+    //3. looping 
+    let result_string="";
+    let temp_index=0
+    for(let index=0;index<col;index++){
+        temp_index=index
+        result_string+=s[temp_index]
         
+        for(let i=0;i<row;i++)
+        {
+            temp_index+=col
+            // console.log(temp_index,s[temp_index])
+            if(temp_index<s.length)
+            {result_string+=s[temp_index]}
+        }
+
+        result_string+=" ";
+        console.log(result_string)
     }
     
-    console.log(s.substr(1,3)) 
-    return s.split("",2)
+    return result_string
 }
 
 console.log(encryption("haveaniceday"))
