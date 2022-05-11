a2e4059e09fc933450ddbb3ab4098b584a9212d9
Ahmad Raza <ahmadrazach26@gmail.com>
2022-05-11 13:20:00 +0500

    create the logic for kaprekarNumbers.js

---
+++ Algorithms/kaprekarNumbers.js
@@ -0,0 +1,26 @@
+/*
+ * Complete the 'kaprekarNumbers' function below.
+ *
+ * The function accepts following parameters:
+ *  1. INTEGER p
+ *  2. INTEGER q
+ */
+function kaprekarCheck(num)
+{
+    //3- sqaure the no.
+    //4- Square no. should equal to 2xd or 2xd-1
+    //5- split no. into 2 substrings
+    //6- right substring should be equal to the digit
+    //7- convert substrings to int, add them to see if we get the n
+
+    //8- return true or false w.r.t no. 7
+}
+function kaprekarNumbers(p, q) {
+    // Write your code here
+    
+    //1- Loop from range p,q
+    //2- take number as temp
+
+}
+
+console.log(kaprekarNumbers(1,100))
