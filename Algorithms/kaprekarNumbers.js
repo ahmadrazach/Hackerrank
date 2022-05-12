9e8cabf14332345f6ab28ab580c75ce72919336d
Ahmad Raza <ahmadrazach26@gmail.com>
2022-05-12 11:15:19 +0500

    done with the logic with syntax of kaprekarNumbers.js

--- Algorithms/kaprekarNumbers.js
+++ Algorithms/kaprekarNumbers.js
@@ -9,38 +9,62 @@ function kaprekarCheck(num)
 {
     //3- sqaure the no.
     let square= (num*num)
-    console.log('square',square)
+    // console.log('square',square)
     let num_digits=num.toString().length
     //4- Square no. should equal to 2xd or 2xd-1
-    console.log ( ((square.toString().length)===(2*num_digits)) || ((square.toString().length)===(2*num_digits-1)))
-    //5- split no. into 2 substrings
-    // console.log(square.toString().match(/.{1}/g))
-    size=(square.toString().length/2).toString()
-    console.log(size)
-    var regex = new RegExp(".{"+ size+"}", 'g' );
-    let substrings=square.toString().match(regex)
-    console.log(substrings)
-    //6- right substring should be equal to the digit
-    console.log("right->",num_digits===substrings[1].length)
-
-    //7- convert substrings to int, add them to see if we get the n
-    console.log((parseInt(substrings[0])+parseInt((substrings[1]))===num))
+    if( ((square.toString().length)===(2*num_digits)) || ((square.toString().length)===(2*num_digits-1)))
+    {
+        //5- split no. into 2 substrings
+        // console.log(square.toString().match(/.{1}/g))
+        size=parseInt(square.toString().length/2).toString()
+        // console.log(size)
+        // console.log(size)
+        var regex = new RegExp(".{"+ size+"}", 'g' );
+        let substrings=square.toString().match(regex)
+        // console.log(substrings)
+        //6- right substring should be equal to the digit
+        if(num_digits===substrings[1].length)
+        {
+            //7- convert substrings to int, add them to see if we get the n
+            if((parseInt(substrings[0])+parseInt((substrings[1]))===num))
+            {
+                return true
+            }
+            return false
+        }
+        else return false
+        
+        
+        //8- return true or false w.r.t no. 7
+    }
+    return false
     
-    return num
-    //8- return true or false w.r.t no. 7
+   
+    
 }
 function kaprekarNumbers(p, q) {
     // Write your code here
-    
+    let result_string="";
     //1- Loop from range p,q
     for(let num=p;num<=q;num++)
     {
+        
+        // console.log(num)
         //2- take number as temp and pass it to function
-        console.log(kaprekarCheck(num))
-        //9- if returns true ,add the no in string + space
+        if(kaprekarCheck(num)===true || num ===1)
+        {
+            //9- if returns true ,add the no in string + space
+            result_string+=num+" ";
+        }
+        
+        
     }
-     
     //10- if string.length>1 return string else INVALID 
+    if(result_string.length>0)
+    return result_string
+    return "INVALID RANGE"
+
+    
 }
-console.log(kaprekarCheck(98))
-// console.log(kaprekarNumbers(1,100))
+// console.log(kaprekarCheck(99))
+console.log(kaprekarNumbers(1,100))
