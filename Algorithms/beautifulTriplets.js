4a69781f300ea5c9627676398d6b8d287057fe57
Ahmad Raza <ahmadrazach26@gmail.com>
2022-05-14 12:18:06 +0500

    done with passing all the test cases

--- Algorithms/beautifulTriplets.js
+++ Algorithms/beautifulTriplets.js
@@ -6,16 +6,39 @@
  *  1. INTEGER d
  *  2. INTEGER_ARRAY arr
  */
-
+// MY APPROACH
 function beautifulTriplets(d, arr) {
     // Write your code here
-
-    //1- sort the array
-    //2- loop of i from 0 - arr.length-2
-    //3- loop of j from i+1 - arr.length-1
-    //4- loop of k from j+1 - arr.length
-    //5- check if arr[j]-arr[i]=arr[k]-arr[j]=d  ? count+=1
-    //return count
+    let count=0;
+    //1- loop of i from 0 - arr.length-2
+    for(let i=0;i<arr.length-2;i++)
+        //2- loop of j from i+1 - arr.length-1
+        for(let j=i+1;j<arr.length-1;j++)
+            //3- loop of k from j+1 - arr.length
+            for(let k=j+1;k<arr.length;k++)
+             //4- check if arr[j]-arr[i]=arr[k]-arr[j]=d  ? count+=1
+              if(arr[j]-arr[i]===d & arr[k]-arr[j]===d)
+                count++;
+    
+    
+   
+    return count
 }
 
-console.log(beautifulTriplets(1,[2,2,3,4,5]))
+
+// Efficient approach
+// function beautifulTriplets(d, arr) {
+//     let result = 0;
+//     // traverse through all the array
+//     for (let i = 0; i < arr.length; i++) {
+//         //taking n0.s which should be there if arr[j]-arr[i]=d =arr[k]-arr[j]
+//         const numb1 = arr[i] - d;//shouled be arr[j]
+//         const numb2 = numb1 - d;//should be arr[k]
+//         if (arr.includes(numb1) && arr.includes(numb2)) {
+//             result += 1;
+//         }
+//     }
+//     return result;
+// }
+
+console.log(beautifulTriplets(3,[1, 6, 7, 7, 8 ,10, 12, 13, 14 ,19]))
