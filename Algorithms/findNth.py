8dedf0336b1452edfe7b32e6a7150ba4ea3354cc
Ahmad Raza <ahmadrazach26@gmail.com>
2022-05-16 14:08:02 +0500

    update

--- Algorithms/findNth.py
+++ Algorithms/findNth.py
@@ -1,7 +1,38 @@
-def findNth(num):
-    # 1- loop till all i=1 to i<=num
-    # 2- check if digit is in the i 
-    # 3- yes? count it : no pass
-    # 4- return the number
 
-print(findNth(9))
+def findNth(num):
+    count=0
+    # 1- loop till all i=1 to i<=num
+    for i in range(1,int(1e+12)):
+        # Nth_num=0
+        
+    # 2- check if digit is in the i
+        
+        check_num=str(i)
+        # print("i->",i)
+        if not('9' in check_num):
+            # print("yes 9",check_num)
+            # pass
+            count+=1
+        # else:
+            # print("not 9",check_num)
+            # count+=1
+            # print("count->",count)
+        # for j in check_num:
+        # #     print(j,end="")
+
+        #     if ('9' in j):
+        # #         print("counted")
+        #         pass
+        #     else:
+        #         count+=1
+        # # if check_num.index('4')==1:
+        # print()
+        # 3- yes? count it : no pass
+            # print(i)
+            # count+=1
+        if count==num :
+            return (i)
+    # 4- return the number
+    
+
+print(findNth(18))
