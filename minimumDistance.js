e51ccd4cc469855bbe194532bd13365bb71af09a
Ahmad Raza <ahmadrazach26@gmail.com>
2022-05-19 18:18:36 +0500

    Changed into the syntax

--- minimumDistance.js
+++ minimumDistance.js
@@ -8,9 +8,20 @@
 function minimumDistances(a) {
     // Write your code here
     //  let min_dis=-1
+    let min_dist=10000
     // 1- take a loop i=0 to a.length-1
+    for(let i=0;i<a.length-1;i++)
     // 2- take a loop from j=i+1 to a.length
-    // 3- if a[j]-a[i]===0 && abs(j-i)<min ? min=abs(j-1):pass 
+        for(let j=i+1;j<a.length;j++)
+    // 3- if a[j]-a[i]===0 && abs(j-i)<min ? min=abs(j-1):pass
+            if(a[j]-a[i]===0 && Math.abs(j-i)<min_dist)
+            {
+                min_dist=Math.abs(j-i)
+            } 
+
+    if(min_dist===10000)
+    console.log(-1)
+    console.log(min_dist)
 }
 
-minimumDistances([3,2,1,2,3])
+minimumDistances([7, 1, 3, 4, 1, 7])
