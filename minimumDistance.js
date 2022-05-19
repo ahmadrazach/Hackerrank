b73cfce2d022b8f883a636ff2abeffe1d35fd793
Ahmad Raza <ahmadrazach26@gmail.com>
2022-05-19 18:09:46 +0500

    made logic of the minimumDistance.js

---
+++ minimumDistance.js
@@ -0,0 +1,16 @@
+/*
+ * Complete the 'minimumDistances' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+function minimumDistances(a) {
+    // Write your code here
+    //  let min_dis=-1
+    // 1- take a loop i=0 to a.length-1
+    // 2- take a loop from j=i+1 to a.length
+    // 3- if a[j]-a[i]===0 && abs(j-i)<min ? min=abs(j-1):pass 
+}
+
+minimumDistances([3,2,1,2,3])
