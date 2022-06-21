f2de6fa674c7342c5e38971c16695bab1d7edb69
Ahmad Raza <ahmadrazach26@gmail.com>
2022-06-21 16:52:26 +0500

    done with solution with complexity  O(N) of a solution

--- Data Structures/arrayManipulation.js
+++ Data Structures/arrayManipulation.js
@@ -5,8 +5,11 @@
  * The function accepts following parameters:
  *  1. INTEGER n
  *  2. 2D_INTEGER_ARRAY queries
+ * 
+ * optimized code help : https://medium.com/@Kliative/array-manipulation-hacker-rank-javascript-5a702c1ff044
  */
 
+/*  solution with complexity O(n^2)
 function arrayManipulation(n, queries) {
     // Write your code here
     //1- make a 1d array of size n
@@ -29,5 +32,33 @@ function arrayManipulation(n, queries) {
     return arr.reduce((a,b)=>{return Math.max(a,b)})
 
 }
+*/
+// Complexity -> O(n)
+function arrayManipulation(n,queries){
+    //1- making a 1d array
+    let arr = [...Array(n).fill(0)];
+    let maxValue = 0,
+    currentNumber = 0;
 
+    //2- loop to each query and giving start and end range value 
+    queries.forEach(([startRange, endRange, value]) => {
+         arr[startRange] = arr[startRange] || { start: 0, end: 0 };
+         arr[endRange] = arr[endRange] || { start: 0, end: 0 };
+         arr[startRange].start += value;
+         arr[endRange].end += value;
+     });
+    
+     //3- loop to each arr iteration to check where the maximum value lies w.r.t iteration
+    arr.forEach((cell) => {
+       if (cell) {
+         currentNumber += cell.start;
+         if (currentNumber > maxValue) {
+            maxValue = currentNumber;
+         }
+         currentNumber -= cell.end;
+     }
+   });
+//4- returning maxValue
+ return maxValue;
+}
 console.log(arrayManipulation(5, [[1,2,100],[2,5,100],[3,4,100]]));
