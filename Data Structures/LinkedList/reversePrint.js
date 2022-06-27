3281f978645f10be7796c6a6ee34e4a0147865b0
Ahmad Raza <ahmadrazach26@gmail.com>
2022-06-27 19:33:54 +0500

    Create reversePrint.js
    Done with the question

---
+++ Data Structures/LinkedList/reversePrint.js
@@ -0,0 +1,65 @@
+/*
+ * Complete the 'reversePrint' function below.
+ *
+ * The function accepts INTEGER_SINGLY_LINKED_LIST llist as parameter.
+ */
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+
+const SinglyLinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
+
+const SinglyLinkedList = class {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    insertNode(nodeData) {
+        const node = new SinglyLinkedListNode(nodeData);
+
+        if (this.head == null) {
+            this.head = node;
+        } else {
+            this.tail.next = node;
+        }
+
+        this.tail = node;
+    }
+};
+
+function printSinglyLinkedList(node, sep) {
+    while (node != null) {
+        process.stdout.write(String(node.data));
+
+        node = node.next;
+
+        if (node != null) {
+            process.stdout.write(sep);
+        }
+    }
+}
+
+function reversePrint(llist) {
+    // Write your code here
+    if(!head)
+    {
+        return ;
+    }
+    else{
+        reversePrint(llist.next);
+        console.log(list.data);
+    }
+}
